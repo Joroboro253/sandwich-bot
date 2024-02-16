42c7986cfc8070ea8337c003060c72392989e75f
Vitaliy Lyashchenko <vlyashchenko01@gmail.com>
2024-02-17 00:25:27 +0200

    uniswap detection

--- internal/service/subscribeToUniswapEvents.go
+++ internal/service/subscribeToUniswapEvents.go
@@ -2,34 +2,38 @@ package service
 
 import (
 	"context"
-	"fmt"
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"log"
 )
 
 func SubscribeToUniswapEvents(client *ethclient.Client) {
-	headers := make(chan *types.Header)
-	sub, err := client.SubscribeNewHead(context.Background(), headers)
+	swapSignature := []byte("Swap(address,uint256,uint256,uint256,uint256,address)")
+	swapSigHash := crypto.Keccak256Hash(swapSignature)
+
+	// Subscription on logs
+	logs := make(chan types.Log)
+	sub, err := client.SubscribeFilterLogs(context.Background(), ethereum.FilterQuery{
+		Topics: [][]common.Hash{{swapSigHash}},
+	}, logs)
 	if err != nil {
-		log.Fatalf("Couldn`t subscribe to new block headers")
+		log.Fatal(err)
 	}
 
-	fmt.Println("Subscription on new transaction...")
 	for {
 		select {
 		case err := <-sub.Err():
 			log.Fatal(err)
-		case header := <-headers:
-			fmt.Printf("New block: #%v\n", header.Number.String())
-			block, err := client.BlockByHash(context.Background(), header.Hash())
-			if err != nil {
-				log.Fatalf("Getting a block failed: %v", err)
-			}
-
-			for _, tx := range block.Transactions() {
-				fmt.Printf("New transaction: %s\n", tx.Hash().Hex())
-				//  transaction analyze
+		case vLog := <-logs:
+			if len(vLog.Topics) == 3 { // Uniswap v2 Swap have 2 indexed parameters + event signature
+				if err != nil {
+					log.Printf("Error decoding swap event: %s", err)
+					continue
+				}
+				log.Printf("Detected Uniswap v2 Swap event: %v\n", vLog)
 			}
 		}
 	}

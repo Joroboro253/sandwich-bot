45f18ab5fcf1968cdda8546e2e5cbbdf43d14442
Vitaliy Lyashchenko <69591510+Joroboro253@users.noreply.github.com>
2024-02-21 16:07:26 +0200

    searching uniswap transactions in mempool

--- internal/service/helpers/subscribe_to_pending_transactions.go
+++ internal/service/helpers/subscribe_to_pending_transactions.go
@@ -3,23 +3,29 @@ package helpers
 import (
 	"context"
 	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/rpc"
 	"log"
 )
 
-func SubscribeToPendingTransactions(rpcClient *rpc.Client) {
-	ch := make(chan string)
-	_, err := rpcClient.Subscribe(context.Background(), "eth", ch, "newPendingTransactions")
+func SubscribeToPendingTransactions(rpcClient *rpc.Client, ethClient *ethclient.Client) {
+	ch := make(chan common.Hash)
+	sub, err := rpcClient.EthSubscribe(context.Background(), ch, "newPendingTransactions")
 	if err != nil {
 		log.Fatalf("Failed to subscribe to new pending transactions: %v", err)
 	}
+	defer sub.Unsubscribe()
 
 	fmt.Println("Subscribed to new pending transactions")
+
 	for {
 		select {
+		case err := <-sub.Err():
+			log.Fatalf("Subscription error: %v", err)
 		case txHash := <-ch:
-			fmt.Println("New pending transaction:", txHash)
-			// Здесь вы можете добавить дополнительную логику для фильтрации и анализа транзакций
+			//fmt.Println("New pending transaction:", txHash.Hex())
+			analyzeTransaction(txHash, ethClient) // Анализ транзакции
 		}
 	}
 }
